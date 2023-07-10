897cd306142f396d779b0b923f08af355b5b430a
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-11 01:00:27 +0300

    feat: describe styles, add normalize and fonts

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.scss';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
