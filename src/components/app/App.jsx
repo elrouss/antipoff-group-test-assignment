8efae3ead4bde985a914d462e6400d597fe94f8f
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-11 15:25:23 +0300

    feat: markup dynamic route with team member info

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import OurTeamPage from '../../pages/our-team';
+import TeamMemberDetailsPage from '../../pages/team-member-details';
 
 import { ROUTES } from '../../utils/constants';
 
@@ -8,6 +9,10 @@ function App() {
   return (
     <Routes>
       <Route path={ROUTES.ourTeam.main} element={<OurTeamPage />} />
+      <Route
+        path={`${ROUTES.ourTeam.main}${ROUTES.ourTeam.member}`}
+        element={<TeamMemberDetailsPage />}
+      />
     </Routes>
   );
 }
