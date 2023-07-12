158b00121254701dd6d3c37e5d7a20bdc75a87f4
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-07-12 17:14:20 +0300

    feat: create logic for showing more cards on button click

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -20,3 +20,10 @@ export const API = Object.freeze({
 
 // MEDIA QUERIRES (see also scss styles)
 export const MEDIA_TABLET = 768;
+
+// CARDS IN SECTION OUR TEAM
+export const NUM_CARDS_DESKTOP_INIT = 6;
+export const NUM_CARDS_TABLET_INIT = 4;
+
+export const NUM_CARDS_DESKTOP_ADD = 3;
+export const NUM_CARDS_TABLET_ADD = 1;
